b7dc25fa4fcb2ad9ad89f1bceb349b09005ef69f
Jerome Paulos <jeromepaulos@yahoo.com>
2022-09-04 23:58:45 -0500

    Remove calendars store

--- src/routes/calendar/+page.server.js
+++ src/routes/calendar/+page.server.js
@@ -1,6 +1,6 @@
 import parseICS from 'ical';
-import { calendar } from '../../lib/stores.js';
-import { get } from 'svelte/store';
+// import { calendar } from '../../lib/stores.js';
+// import { get } from 'svelte/store';
 
 // 4 = clubs
 // 5 = sports
@@ -10,9 +10,10 @@ import { get } from 'svelte/store';
 // 24 = films & videos
 
 export async function load(event) {
-    let events = get(calendar);
+    // let events = get(calendar);
+    let events = [];
 
-    if(calendar.length === 0) {
+    // if(calendar.length === 0) {
         for(const calendarId of [4, 5, 7, 8, 15, 24]) {
             const request = await fetch(`http://webapps.macalester.edu/eventscalendar/events/ical/?calendarId=${calendarId}`);
             const raw = await request.text();
@@ -27,8 +28,8 @@ export async function load(event) {
         }
     
         events.sort((a, b) => new Date(a.start) - new Date(b.start));
-        calendar.set(events);
-    }
+        // calendar.set(events);
+    // }
 
     return { events: events };
 }
