d38f075bb233d397a26315c85c1f8024948cfc7c
Jerome Paulos <jeromepaulos@yahoo.com>
2022-09-04 23:56:31 -0500

    Fix import

--- src/routes/calendar/+page.server.js
+++ src/routes/calendar/+page.server.js
@@ -1,4 +1,4 @@
-import { parseICS } from 'ical';
+import parseICS from 'ical';
 import { calendar } from '../../lib/stores.js';
 import { get } from 'svelte/store';
 
@@ -12,7 +12,7 @@ import { get } from 'svelte/store';
 export async function load(event) {
     let events = get(calendar);
 
-    if(!calendar.length) {
+    if(calendar.length === 0) {
         for(const calendarId of [4, 5, 7, 8, 15, 24]) {
             const request = await fetch(`http://webapps.macalester.edu/eventscalendar/events/ical/?calendarId=${calendarId}`);
             const raw = await request.text();
