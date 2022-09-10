eedd08647a1367855da5073a8ed9d04f89fc95ca
Jerome Paulos <jeromepaulos@yahoo.com>
2022-09-10 17:13:17 -0500

    Try to add caching

--- src/routes/calendar/+page.server.js
+++ src/routes/calendar/+page.server.js
@@ -10,27 +10,25 @@ import ical from 'ical';
 // 15 = campus
 // 24 = films & videos
 
-export async function load(event) {
-    // let events = get(calendar);
+export async function load({ setHeaders }) {
     let events = [];
 
-    // if(calendar.length === 0) {
-        for(const calendarId of [4, 5, 7, 8, 15, 24]) {
-            const request = await fetch(`http://webapps.macalester.edu/eventscalendar/events/ical/?calendarId=${calendarId}`);
-            const raw = await request.text();
-    
-            let parsedData = Object.values(ical.parseICS(raw)).map(event => {
-                event.calendarId = calendarId;
-                return event;
-            });
-    
-            parsedData.shift();
-            events.push(...parsedData);
-        }
-    
-        events.sort((a, b) => new Date(a.start) - new Date(b.start));
-        // calendar.set(events);
-    // }
+    for(const calendarId of [4, 5, 7, 8, 15, 24]) {
+        const request = await fetch(`http://webapps.macalester.edu/eventscalendar/events/ical/?calendarId=${calendarId}`);
+        const raw = await request.text();
+
+        let parsedData = Object.values(ical.parseICS(raw)).map(event => {
+            event.calendarId = calendarId;
+            return event;
+        });
+
+        parsedData.shift();
+        events.push(...parsedData);
+    }
+
+    events.sort((a, b) => new Date(a.start) - new Date(b.start));
+
+    setHeaders({ 'cache-control': 'stale-while-revalidate=3600' });
 
     return { events: events };
 }
