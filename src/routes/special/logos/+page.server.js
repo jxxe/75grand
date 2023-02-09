63eb5724697cf20d4351a6dbcda884f7395a11b2
Jerome Paulos <jeromepaulos@yahoo.com>
2023-02-08 23:29:46 -0600

    Fix Raindrop page

--- src/routes/special/logos/+page.server.js
+++ src/routes/special/logos/+page.server.js
@@ -4,11 +4,12 @@ import { RAINDROP_API_TOKEN } from '$env/static/private';
 import { getCacheUrl } from '$lib/helpers.js';
 
 export async function load(event) {
-    const request = await fetch(getCacheUrl('https://api.raindrop.io/rest/v1/raindrops/28435309'), {
+    const request = await fetch('https://api.raindrop.io/rest/v1/raindrops/28435309', {
+        method: 'GET',
         headers: {
             Authorization: `Bearer ${RAINDROP_API_TOKEN}`
         }
-    }).catch(console.log);
+    }).catch(console.error);
 
     const response = await request.json();
 
